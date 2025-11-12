30a18ad973ee1741450be8a34e592f6bfaab5708
Charlie Phillips <charlie_l_phillips@proton.me>
2025-11-11 19:11:46 -0500

    tof fix?

--- src/tof.rs
+++ src/tof.rs
@@ -51,11 +51,11 @@ pub fn init_tof() -> Vl53l1x {
 }
 
 pub fn tof_eq_int(_event: Event, tof_sensor: Arc<Mutex<Vl53l1x>>, cur_roi: &ROIRight, cur_eq3: &AtomicU16, enabled: &Arc<AtomicBool>) {
+    //println!("TOF interrupt");
+    let mut sensor = tof_sensor.lock().expect("failed to acquire sensor lock");
+    let sample = sensor.read_sample().expect("failed to get right sample");
+    println!("sampled: {}mm ({:#?})", sample.distance, sample.status);
     if enabled.load(std::sync::atomic::Ordering::SeqCst) {
-        //println!("TOF interrupt");
-        let mut sensor = tof_sensor.lock().expect("failed to acquire sensor lock");
-        let sample = sensor.read_sample().expect("failed to get right sample");
-        println!("sampled: {}mm ({:#?})", sample.distance, sample.status);
         match sample.status {
             Vl53l1xRangeStatus::Ok => {
                 let filter_strength: i8 = if sample.distance < 300 {
