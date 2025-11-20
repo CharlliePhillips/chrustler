5ef10f4b3743d626036f7d39d3ad854f685ffc2e
Charlie Phillips <charlie_l_phillips@proton.me>
2025-11-20 17:49:12 -0500

    update autostart script

--- autostart/chrustler_autostart.sh
+++ autostart/chrustler_autostart.sh
@@ -3,8 +3,6 @@
 # this will not do anything unless called by a .desktop file
 aconnect -x
 pulseaudio --start
-echo '20s'
-env sleep 10
-echo '10s'
-env sleep 10
+echo '5s'
+env sleep 5
 /home/charlie/chrustler/target/release/chrustler >> /home/charlie/chrustler.log 2>&1
